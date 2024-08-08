ef68a08a6dc4727dd18042b8ce4587ae4d4f5c2d
Chamiththree16 <chamiththreejayasinghe@gmail.com>
2024-08-08 06:19:02 +0530

    Create Arithmatic.java
    Add Arithmatic class

---
+++ Arithmatic.java
@@ -0,0 +1,19 @@
+class Arithmatic{
+	public static void main(String [] args){
+	int a=7;
+	double b=4;
+	
+	System.out.println("a ="+a+ " "+ "b ="+b);
+	System.out.println("a+b =" +(a+b));
+	System.out.println("a-b =" +(a-b));
+	System.out.println("a*b =" +(a*b));
+	System.out.println("a/b =" +(a/b));
+	System.out.println("a%b =" +(a%b));
+	
+	
+	
+	}
+
+
+
+}
