50ac7859107626984c6dac8aedce20f7724712f0
Chamiththree16 <chamiththreejayasinghe@gmail.com>
2024-08-08 06:18:09 +0530

    Create Addition.java
    Add simple Addition

---
+++ Addition.java
@@ -0,0 +1,21 @@
+class Addition{
+
+ static int add(int a,int b){
+	return a+b;
+
+}
+public static void main(String args[]){
+
+	int number1 =10;
+	int number2= 20;
+	
+	
+	System.out.println("Addition of 2 numbers is : " +add(number1,number2));
+
+
+
+
+	}
+
+
+}
